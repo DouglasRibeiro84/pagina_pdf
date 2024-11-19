7bf5ad65ebf374a1a309541c1a0687f14eb7eb9c
doouglas <douglasribeiro084@gmail.com>
2024-11-19 07:40:35 -0300

    Finalizado a configuração

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>
+        <div class="container">
+            <h1 class="text-center">
+                ola
+            </h1>
+        </div>
+    </h1>
+</template>
